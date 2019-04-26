9cd53e91cc7379191c919776922e13f9d5add4c4
Faye Xue <fxue89@gmail.com>
2019-04-26 16:43:34 +1000

    modify steps of rating collector

--- src/components/RatingCollector/RatingCollector.tsx
+++ src/components/RatingCollector/RatingCollector.tsx
@@ -1,5 +1,7 @@
 // 1. use {gql} from "apollo-boost"
-// 2. write your own mutation by following the formate:
+// 2. write query for movieUserRating, defined in query.ts
+// 3. pass GraphQL query into Query component as the field of query
+// 4. write your own mutation by following the formate:
 /* 
 mutation mutationName($variableName: type) {
  mutation content
@@ -8,9 +10,9 @@ mutation mutationName($variableName: type) {
   }
 } 
 */
-// 3. use { Mutation } component provided by "react-apollo"
-// 4. pass GraphQL mutation into Mutation component as the field of mutation
-// 5. You can use your mutation within the mutation component as props
+// 5. use { Mutation } component provided by "react-apollo"
+// 6. pass GraphQL mutation into Mutation component as the field of mutation
+// 7. You can use your mutation within the mutation component as props
 
 import * as React from "react";
 import { useRef, useState, useEffect } from "react";
@@ -18,13 +20,6 @@ import Star from "@material-ui/icons/Star";
 import StarBorder from "@material-ui/icons/StarBorder";
 import { getUserId } from "../../utils/userIdHelper";
 import RatingStars from '../UIComponents/RatingStars';
-
-// TODO:
-// 1. write query for movieUserRating, defined in query.ts
-// 2. pass GraphQL query into Query component as the field of query
-// 3. Handle loading/error situations
-// 4. write mutation for setRating, define mutation in mutation.ts
-// 5. create the functions to execute on setRating.
 
 const RatingCollector: React.FC<{ movieId: string }> = ({ movieId }) => {
   const [score, setRatingScore] = useState<number | undefined>(undefined);
