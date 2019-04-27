8d1dd2c9d5d64668813c087598addc030d01285b
Faye Xue <fxue89@gmail.com>
2019-04-28 09:56:31 +1000

    set rating only

--- src/components/RatingCollector/RatingCollector.tsx
+++ src/components/RatingCollector/RatingCollector.tsx
@@ -1,7 +1,5 @@
 // 1. use {gql} from "apollo-boost"
-// 2. write query for movieUserRating, defined in query.ts
-// 3. pass GraphQL query into Query component as the field of query
-// 4. write your own mutation by following the formate:
+// 2. write your own mutation by following the formate:
 /* 
 mutation mutationName($variableName: type) {
  mutation content
@@ -10,9 +8,9 @@ mutation mutationName($variableName: type) {
   }
 } 
 */
-// 5. use { Mutation } component provided by "react-apollo"
-// 6. pass GraphQL mutation into Mutation component as the field of mutation
-// 7. You can use your mutation within the mutation component as props
+// 3. use { Mutation } component provided by "react-apollo"
+// 4. pass GraphQL mutation into Mutation component as the field of mutation
+// 5. You can use your mutation within the mutation component as props
 
 import * as React from "react";
 import { useState } from "react";
